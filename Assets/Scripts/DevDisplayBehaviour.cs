d86c46e3b74dbe281c24c862857aaf886f3c0815
twds0x13 <hunter20913@qq.com>
2025-06-04 15:46:46 +0800

    0

--- Assets/Scripts/DevDisplayBehaviour.cs
+++ Assets/Scripts/DevDisplayBehaviour.cs
@@ -5,20 +5,14 @@ using Game = GameBehaviourManager.GameBehaviour;
 
 public class DevDisplayBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public TextMeshProUGUI Text;
-
     void Start()
     {
-        Text = transform.GetComponent<TextMeshProUGUI>();
-        // TxtCurrentTime = GetComponent<TextMeshPro> ();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //获取系统当前时间
-
         Text.text = NowTime.ToString("HH:mm");
     }
 }
